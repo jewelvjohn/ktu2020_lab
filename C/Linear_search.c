892497e0df6053a0f2fa648ba4bafa0b336c8e82
Jewel John <jeweljohnl0v3@gmail.com>
2023-02-28 20:21:39 +0530

    Arranged C programs

---
+++ C/Linear_search.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int n,Array[100],i,num,flag = 0;
+	printf("Enter The number of elements : ");
+	scanf("%d", &n);
+	printf("Enter the %d elements of the array : " ,n);
+	for (i=0; i<n; i++)
+	{
+		scanf("%d", &Array[i]);
+	}
+	printf("Enter the element to search : ");
+	scanf("%d", &num);
+	
+	for (i=0; i<n; i++)
+	{
+		if(num == Array[i])
+		{
+			printf("The element is found at %d position" ,i+1);
+			flag = 1;
+		}
+	}
+	if(flag == 0)
+    {
+	   	printf("The element is not found!");
+   	}
+	return 0;	
+}
